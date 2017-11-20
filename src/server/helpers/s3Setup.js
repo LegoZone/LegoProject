7a6f75b45fbf1035efa144f7449c58c096beeace
Esraa Jbara <jbesraa@gmail.com>
2017-11-20 10:41:42 +0200

    Update s3Setup.js

--- src/server/helpers/s3Setup.js
+++ src/server/helpers/s3Setup.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk')
 
 AWS.config.region = 'us-west-2'
-AWS.config.accessKeyId = 'AKIAIOWNN7P5A5LKSQXQ'
-AWS.config.secretAccessKey = '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'
+AWS.config.accessKeyId = 
+AWS.config.secretAccessKey = 
 
 const s3 = new AWS.S3()
 
