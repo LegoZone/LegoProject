c118b91f9088e07b4418fb34618ead1ffbb5bb0f
mario <mario.saliba98@gmail.com>
2017-11-28 09:56:18 +0200

    adds admin and login hbs files and handlers

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,8 +4,13 @@ const homepage = require('./homepage.js');
 const packages = require('./packages.js');
 const { legoHandler } = require('./lego.js');
 const redisRequest = require('./redis_request.js');
+const adminHandler = require('./admin.js');
+const loginHandler = require('./login.js');
 
 router.get('/', homepage);
+router.get('/admin', adminHandler);
+router.get('/login', (req, res) => res.render('login'));
+router.post('/login', loginHandler);
 router.get('/packages', packages);
 router.get('/lego', legoHandler);
 router.post('/redisRequest', redisRequest);
