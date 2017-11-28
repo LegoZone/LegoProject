77c7f1d0197dbc0e17c2d799afce962f729f68ec
mario <mario.saliba98@gmail.com>
2017-11-28 14:48:56 +0200

    changes singup route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,18 +1,25 @@
 const router = require('express').Router();
 
+// controllers
 const homepage = require('./homepage.js');
 const packages = require('./packages.js');
 const { legoHandler } = require('./lego.js');
 const redisRequest = require('./redis_request.js');
-const adminHandler = require('./admin.js');
+const signupHandler = require('./signup.js');
 const loginHandler = require('./login.js');
+const renderLoginHandler = require('./renderLogin');
 
+// middleware
+const authenticate = require('../middleware/authenticate.js');
+
+// routes
 router.get('/', homepage);
-router.get('/admin', adminHandler);
-router.get('/login', (req, res) => res.render('login'));
+router.get('/admin', (req, res) => res.render('admin'));
+router.post('/admin', signupHandler);
+router.get('/login', renderLoginHandler);
 router.post('/login', loginHandler);
 router.get('/packages', packages);
-router.get('/lego', legoHandler);
+router.get('/lego', authenticate, legoHandler);
 router.post('/redisRequest', redisRequest);
 
 module.exports = router;
