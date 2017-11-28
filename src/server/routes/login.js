dd395d05d98833bd7a3bf59332bda3b59d2a51e8
mario <mario.saliba98@gmail.com>
2017-11-28 14:49:35 +0200

    modify the login functionality

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -1,43 +1,31 @@
 const { users } = require('../model/schema.js');
 const bcrypt = require('bcrypt');
-
-function handleError(res, message) {
-  res.render('login', {
-    error: message
-  });
-}
-
-function validate(username, password, res) {
-  const englishChars = /[a-zA-Z0-9]/;
-
-  if (!username.trim() || !password.trim()) {
-    handleError(res, 'Please no white spaces');
-  } else if (!englishChars.test(username) || !englishChars.test(password)) {
-    handleError(res, 'Please use english characters and number only');
-  } else {
-    return;
-  }
-}
+const jwt = require('jsonwebtoken');
+const { validateLogin, handleError } = require('../helpers/validation.js');
+require('env2')('config.env');
 
 module.exports = async ({ body }, res) => {
   const { username, password } = body;
 
   try {
-    validate(username, password, res);
+    validateLogin(username, password, res);
     const user = await users.find({ username });
     if (user.length === 0) {
-      handleError(res, 'This user does not exist');
+      handleError('login', res, 'This user does not exist');
     } else {
       const comparision = await bcrypt.compare(password, user[0].password);
 
       if (!comparision) {
-        handleError(res, 'The password that you typed is incorrect');
+        handleError('login', res, 'The password that you typed is incorrect');
       } else {
-        res.cookie('token', user[0].username, { maxAge: 604800000 });
+        const token = jwt.sign(user[0].username, process.env.JWT_SECRET);
+        res.cookie('token', token, {
+          maxAge: 604800000
+        });
         res.redirect('/packages');
       }
     }
   } catch (err) {
-    handleError(res, 'Something went wrong please try again');
+    handleError('login', res, 'Something went wrong please try again');
   }
 };
