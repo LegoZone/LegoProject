5b1010d4922400786253bf78ce6f40c9bf805393
mario <mario.saliba98@gmail.com>
2017-11-28 09:55:48 +0200

    adds qa schema for the users

--- src/server/model/schema.js
+++ src/server/model/schema.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 
-const packagesSchema = new mongoose.Schema({
+const packagesSchema = mongoose.Schema({
   name: { type: String, unique: true, required: true },
   length: { type: String, require: true },
   img: { type: String, required: false }
 });
 
-module.exports = mongoose.model('legoPack', packagesSchema);
+const usersSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = {
+  legoPack: mongoose.model('legoPack', packagesSchema),
+  users: mongoose.model('users', usersSchema)
+};
